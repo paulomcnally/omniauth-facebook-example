5ad210d729f379d0dbdecad6af34c2d342776adc
Paulo McNally <paulomcnally@gmail.com>
2015-04-19 00:13:34 -0600

    Added ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '331992553536283', '18cf03bd12b666be34bad841d21313b6',
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
            :display => 'popup'
 end
